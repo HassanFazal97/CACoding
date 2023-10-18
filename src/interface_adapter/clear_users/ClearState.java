0c5ac16ceae9d391329c66cbf2feca1c49abf696
fazal <fazal.hassan@mail.utoronto.ca>
2023-10-17 23:17:11 -0400

    fixed backwards dependency in ClearState and removed entity import

--- src/interface_adapter/clear_users/ClearState.java
+++ src/interface_adapter/clear_users/ClearState.java
@@ -1,12 +1,9 @@
 package interface_adapter.clear_users;
 
-
-import entity.User;
-
-import java.util.Map;
+import java.util.List;
 
 public class ClearState {
-    private Map<String, User> clearedUsers;
+    private List<String> clearedUsers;
 
     public ClearState(ClearState copy){
         this.clearedUsers = copy.clearedUsers;
@@ -14,18 +11,18 @@ public class ClearState {
 
     public ClearState(){}
 
-    public Map<String, User> getClearedUsers(){
+    public List<String> getClearedUsers(){
         return clearedUsers;
     }
 
-    public void setClearedUsers(Map<String, User> clearedUsers){
+    public void setClearedUsers(List<String> clearedUsers){
         this.clearedUsers = clearedUsers;
     }
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
-        for (Map.Entry<String, User> entry : clearedUsers.entrySet()){
-            sb.append(entry.getKey()).append("\n");
+        for (String username: clearedUsers){
+            sb.append(username).append("\n");
         }
         return sb.toString();
     }
--- src/use_case/clear_users/ClearOutputData.java
+++ src/use_case/clear_users/ClearOutputData.java
@@ -3,16 +3,21 @@ package use_case.clear_users;
 
 import entity.User;
 
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.List;
 
 public class ClearOutputData {
     private final Map<String, User> clearedUsers;
 
+    private final List<String> deletedUsers;
+
     public ClearOutputData(Map<String, User> clearedUsers){
         this.clearedUsers = clearedUsers;
+        this.deletedUsers = new ArrayList<>(clearedUsers.keySet());
     }
 
-    public Map<String, User> getClearedUsers(){
-        return clearedUsers;
+    public List<String> getClearedUsers(){
+        return deletedUsers;
     }
 }
